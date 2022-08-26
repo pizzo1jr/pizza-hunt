eaebc5cb4644aad036c50c66d22b8a03eb7d135e
pizzo1jr <102200085+pizzo1jr@users.noreply.github.com>
2022-08-26 00:55:17 -0400

    final

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -27,7 +27,7 @@ const commentController = {
     Comment.findOneAndUpdate(
         { _id: params.commentId },
         { $push: { replies:body } },
-        { new: true }
+        { new: true, validators: true }
     )
         .then(dbPizzaData => {
             if (!dbPizzaData) {
